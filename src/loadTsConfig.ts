94f437d73c01eb6f0872a43b0c101a0c9652187b
Forbes Lindesay <forbes@lindesay.co.uk>
2019-01-25 17:36:09 +0000

    fix: load compiler options from tsconfig (#4)

--- src/loadTsConfig.ts
+++ src/loadTsConfig.ts
@@ -2,5 +2,15 @@ import {loadSync} from 'tsconfig';
 
 export default function loadTsConfig(cwd: string): any {
   const result = loadSync(cwd);
-  return result.config;
+  const compilerOptions =
+    (result.config && result.config.compilerOptions) || {};
+  if (
+    compilerOptions.experimentalDecorators === false &&
+    compilerOptions.emitDecoratorMetadata === undefined
+  ) {
+    // typescript-json-schema sets emitDecoratorMetadata by default
+    // we need to disable it if experimentalDecorators support is off
+    compilerOptions.emitDecoratorMetadata = false;
+  }
+  return compilerOptions;
 }
