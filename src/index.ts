7128926d36526ef8e59aa76ac6e1b7d584542638
Forbes Lindesay <forbes@lindesay.co.uk>
2020-01-12 14:31:51 +0000

    fix: prettier style formatting

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,7 @@ export {
 export default function run(args?: string[]) {
   const {files, options} = parseArgs(args);
   const tsConfig = loadTsConfig();
-  const parsed = parse(
-    files.map(f => f.fileName),
-    tsConfig,
-    options.schema,
-  );
+  const parsed = parse(files.map(f => f.fileName), tsConfig, options.schema);
 
   files.forEach(({fileName, typeName}) => {
     const outputFileName = fileName.replace(/\.tsx?$/, '.validator.ts');
